d5954378b0d7e52f70aafdd1f1077aa12206b723
João Henrique <joaohenrique.p.almeida@gmail.com>
2023-05-22 11:03:35 +0100

    Add initial config for prisma ORM and create event model migrate

--- dev.Dockerfile
+++ dev.Dockerfile
@@ -2,7 +2,10 @@ FROM node:19-alpine
 
 COPY . .
 WORKDIR /home/node/app
-
+RUN npm i -g @nestjs/cli
 RUN npm install
+
+
+# RUN chmod -R 777 .
 
 EXPOSE 3000
---
+++ prisma/migrations/20230522003139_create_event_model/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Event" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "start_time" TIMESTAMP(3) NOT NULL,
+    "end_time" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Event_pkey" PRIMARY KEY ("id")
+);
